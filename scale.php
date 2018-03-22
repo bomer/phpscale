95ddc2fca7c3cf333649c25965ad35b0b8f69a28
James O <james@au044946>
2018-03-23 00:35:52 +1100

    works now

--- scale.php
+++ scale.php
@@ -1,13 +1,15 @@
 <?php
 //var_dump($_FILES);
-
+$width=$_POST['width'];
 $fname=$_FILES["image"]["tmp_name"];
 $fcontents=file_get_contents($fname);
-file_put_contents("image.jpg",$fcontents);
-$img = new Imagick('image.jpg');
-$img->scaleImage(5000,0);
 
-header("Content-type: image/jpg");
-echo $img->getImageBlob();
+file_put_contents("before.jpg",$fcontents);
+$img = new Imagick('before.jpg');
+$img->scaleImage($width,0);
 
+//header("Content-type: image/jpg");
+//echo $img->getImageBlob();
+$img->writeImage("after.jpg");
+header('Location: index.html');
  ?>
